845ac4143e57aa3d858b371dd8afcb37bc57040b
Andrey_Kuzin <ankuzinx@ya.ru>
2021-10-06 10:52:04 +0300

    Задание №3 неполное

--- main.py
+++ main.py
@@ -27,7 +27,22 @@ class Mentor:
 class Lecturer(Mentor):
     def __init__(self, name, surname):
         super().__init__(name, surname)
-        self.grades = {}
+        self.grades = {'Python': [20, 16, 21], 'GIT': [11, 11, 11]}
+
+    def get_avg_grade(self):
+        sum_lect = 0
+        count = 0
+        for course in self.grades.values():
+            sum_lect += sum(course)
+            count += len(course)
+        return round(sum_lect / count, 2)
+
+    def __str__(self):
+        res = f'Имя: {self.name} \n Фамилия: {self.surname} \n Средняя оценка: {self.get_avg_grade()}'
+        return res
+
+
+
 
 
 class Reviewer(Mentor):
@@ -40,15 +55,38 @@ class Reviewer(Mentor):
         else:
             return 'Ошибка'
 
+    def __str__(self):
+        res = f'Имя: {self.name} \n Фамилия: {self.surname} '
+        return res
 
 
-best_student = Student('Ruoy', 'Eman', 'your_gender')
+cool_reviewer = Reviewer('Some', 'Buddy')
+print(cool_reviewer)
+
+best_student = Student('Red', 'Fury', 'M')
 best_student.courses_in_progress += ['Python']
-cool_lecturer = Lecturer('Some', 'Greg')
+best_student.finished_courses += ['GIT']
+
+some_student = Student('Green', 'Fury', 'M')
+some_student.courses_in_progress += ['Python']
+some_student.finished_courses += ['GIT']
+
+cool_lecturer = Lecturer('Some', 'Buddy')
 cool_lecturer.courses_attached += ['Python']
+cool_lecturer.courses_attached += ['GIT']
 
-best_student.rate_lecture(cool_lecturer, 'Python', 10)
+some_lecturer = Lecturer('Some', 'Buddy')
+some_lecturer.courses_attached += ['Python']
+some_lecturer.courses_attached += ['GIT']
+
+best_student.rate_lecture(cool_lecturer, 'Python', 4)
+best_student.rate_lecture(cool_lecturer, 'GIT', 12)
+
+some_student.rate_lecture(some_lecturer, 'Python', 7)
+some_student.rate_lecture(some_lecturer, 'GIT', 10)
+
+print(some_lecturer)
 
 
-print(cool_lecturer.grades)
+
 
